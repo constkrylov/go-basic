ff80f2e01624a1f5b13aa24755381270da8408d4
Constantine Krylov <kkrylov77@yandex.ru>
2025-11-17 17:03:32 +0300

    ДЗ 4.12

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,6 +7,20 @@ const rateUSDEUR float64 = 0.9
 const rateUSDRUB float64 = 80.0
 const rateEURRUB float64 = rateUSDRUB / rateUSDEUR
 
+func getUserInput() (fromCurrency string, toCurrency string, amount float64) {
+	fmt.Println("Введите исходную валюту:")
+	fmt.Scanln(&fromCurrency)
+	fmt.Println("Введите целевую валюту:")
+	fmt.Scanln(&toCurrency)
+	fmt.Println("Введите количество:")
+	fmt.Scanln(&amount)
+	return fromCurrency, toCurrency, amount
+}
+
+func convertCurrency(fromCurrency string, toCurrency string, amount float64) float64 {
+	return 0.0
+}
+
 func main() {
 	fmt.Printf("Курс USD к RUB %.2f\n", rateUSDRUB)
 	fmt.Printf("Курс USD к EUR %.2f\n", rateUSDEUR)
